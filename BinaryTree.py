a9bc23791c8fd8e7ab9248076d573219d82b8f58
Yakup Aycıl <35653204+yakupaycl@users.noreply.github.com>
2021-11-29 22:04:00 +0300

    Add files via upload

---
+++ BinaryTree.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+1.[7, 5, 1, 8, 3, 6, 0, 9, 4, 2] dizisinin Binary-Search-Tree aşamalarını yazınız. 
+ # root x'dir. root'un sağından y bulunur.
+ Root 5'dir
+
+                    5
+                2(7)
+            (\, /, \)
+          1     3     6    8
+         (\, 0, 4, 9)
+
