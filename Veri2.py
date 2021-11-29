0acb8acce1f247c3f246696ffab80be47c2dbc59
Yakup Aycıl <35653204+yakupaycl@users.noreply.github.com>
2021-11-29 22:03:24 +0300

    Update Veri2.py

--- Veri2.py
+++ Veri2.py
@@ -1,12 +1,7 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
 
 [16,21,11,8,12,22] -> Merge Sort
 
-get_ipython().set_next_input('1. Yukarıdaki dizinin sort türüne göre aşamalarını yazınız');get_ipython().run_line_magic('pinfo', 'yazınız')
+1. Yukarıdaki dizinin sort türüne göre aşamalarını yazınız ?
 
                         [16,21,11,8,12,22]
     
@@ -20,12 +15,7 @@ get_ipython().set_next_input('1. Yukarıdaki dizinin sort türüne göre aşamal
                     \                          /
                          [8,11,12,18,21,22]
                     
-
-
-# In[ ]:
-
-
-get_ipython().set_next_input('2.Big-O gösterimini yazınız');get_ipython().run_line_magic('pinfo', 'yazınız')
+2.Big-O gösterimini yazınız ?
   
     O(nlogn)
 
